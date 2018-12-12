d97b642ca587eb3d55fdbb0a8a5ac24d60adff5f
amundire <valentin.petrov420@abv.bg>
2018-12-12 05:09:17 +0200

    added a scuffed registration form, hooked it to the dark mode toggle script

--- docs/scripts/app.js
+++ docs/scripts/app.js
@@ -21,39 +21,50 @@ function toggledarkmode(){
     let body = document.getElementsByTagName("BODY")[0];
     let h1s = document.querySelectorAll('h1');
     let navlabels = document.querySelectorAll('.nav-label');
+    let registerbutton = document.querySelector('.button');
+    let fieldset = document.querySelector('fieldset');
 
     localStorage.setItem('darkmode', '1')
 
     body.style.cssText = 'background-color: rgba(0, 0, 0, 0.9); color: white;'
-        h1s.forEach( 
-            function(darkmodeh1color, currentIndex) { 
-                h1s[currentIndex].style.cssText = 'color: #b6b6b6';
-            }
-        );
-        navlabels.forEach( 
-            function(darkmodeh1color, currentIndex) { 
-                navlabels[currentIndex].style.cssText = 'color: #b6b6b6';
-            }
-        );
+
+    registerbutton.style.cssText = 'background: #171717; color: #fff;'
+    fieldset.style.cssText = 'background: #41677e;'
+
+    h1s.forEach( 
+        function(darkmodeh1color, currentIndex) { 
+            h1s[currentIndex].style.cssText = 'color: #b6b6b6';
+        }
+    );
+    navlabels.forEach( 
+        function(darkmodeh1color, currentIndex) { 
+            navlabels[currentIndex].style.cssText = 'color: #b6b6b6';
+        }
+    );
 }
 
 function togglelightmode(){
-
     let body = document.getElementsByTagName("BODY")[0];
     let h1s = document.querySelectorAll('h1');
     let navlabels = document.querySelectorAll('.nav-label');
+    let registerbutton = document.querySelector('.button');
+    let fieldset = document.querySelector('fieldset');
 
     localStorage.setItem('darkmode', '0')
 
     body.style.cssText = 'background-color: (255, 255, 255, 0.5); color: black'
-        h1s.forEach( 
-            function(lightmodeh1color, currentIndex) { 
-                h1s[currentIndex].style.cssText = 'color: #363535';
-            }
-        );
-        navlabels.forEach( 
-            function(darkmodeh1color, currentIndex) { 
-                navlabels[currentIndex].style.cssText = 'color: #363535';
-            }
-        );
+
+    registerbutton.style.cssText = 'background: white; color: black;'
+    fieldset.style.cssText = 'background: #9fbbcc;'
+
+    h1s.forEach( 
+        function(lightmodeh1color, currentIndex) { 
+            h1s[currentIndex].style.cssText = 'color: #363535';
+        }
+    );
+    navlabels.forEach( 
+        function(darkmodeh1color, currentIndex) { 
+            navlabels[currentIndex].style.cssText = 'color: #363535';
+        }
+    );
 }
